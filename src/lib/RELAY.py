c4498c9485c3c1b81b3f9da0a8c907202a6522d3
LouisKramer <louis.kramer89@gmail.com>
2026-01-04 12:03:43 +0100

    added logging

--- src/lib/RELAY.py
+++ src/lib/RELAY.py
@@ -5,6 +5,9 @@
 
 from machine import Pin
 import time
+from common.logger import *
+
+log = create_logger("relay", level=LogLevel.INFO)
 
 class Relay:
     """
@@ -28,14 +31,16 @@ class Relay:
         self.off()  # Start in safe (off) state
 
     def on(self):
-        """Turn the relay on (click ON)."""
+        """Turn the relay on"""
+        log.info("Turning relay ON", ctx="relay")
         if self._active_high:
             self._pin.value(1)
         else:
             self._pin.value(0)
 
     def off(self):
-        """Turn the relay off (click OFF)."""
+        """Turn the relay off"""
+        log.info("Turning relay OFF", ctx="relay")
         if self._active_high:
             self._pin.value(0)
         else:
@@ -43,6 +48,7 @@ class Relay:
 
     def toggle(self):
         """Toggle the current relay state."""
+        log.info("Toggling relay state", ctx="relay")
         self._pin.value(not self._pin.value())
 
     def state(self) -> bool:
@@ -59,24 +65,16 @@ class Relay:
             on_time (float): Time in seconds the relay stays ON
             off_time (float): Time in seconds the relay stays OFF
         """
-        print("Starting relay test routine...")
-        print(f"Pin: GPIO{self._pin}, Active {'HIGH' if self._active_high else 'LOW'}")
-        print(f"Performing {cycles} cycles (ON {on_time}s / OFF {off_time}s)\n")
-
-        try:
-            for i in range(cycles):
-                print(f"Cycle {i+1}/{cycles}: ON ", end="")
+        log.info("Starting relay test routine...", ctx="relay-test")
+        log.info(f"Pin: GPIO{self._pin}, Active {'HIGH' if self._active_high else 'LOW'}" , ctx="relay-test")
+        log.info(f"Performing {cycles} cycles (ON {on_time}s / OFF {off_time}s)\n", ctx="relay-test")
+        for i in range(cycles):
+                log.info(f"Cycle {i+1}/{cycles}", ctx="relay-test")
                 self.on()
                 time.sleep(on_time)
-                print("-> OFF")
                 self.off()
                 time.sleep(off_time)
 
-            print("\nRelay test completed successfully!")
-            print("You should have heard the relay clicking and seen the LED change state.\n")
-
-        except KeyboardInterrupt:
-            print("\nTest interrupted by user.")
-        finally:
-            self.off()  # Ensure relay is off when test ends
-            print("Relay forced OFF for safety.")
+        log.info("Relay test completed successfully!", ctx="relay-test")
+        self.off()  # Ensure relay is off when test ends
+        log.info("Relay forced OFF for safety.", ctx="relay-test")
